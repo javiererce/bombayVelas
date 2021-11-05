44f722907ee4c843a677b2aeeeb91df1757ac6e7
John Doe <javiererce@hotmail.com>
2021-11-05 13:02:53 -0300

    subiendo app a github

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenidos a mi ecommerce Bombay velas.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Pagina de React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
