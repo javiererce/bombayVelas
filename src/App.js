fa6ffac4a4e6600fcf52774c411b8fb94293453a
John Doe <javiererce@hotmail.com>
2021-11-11 21:01:54 -0300

    creando navBar

--- src/App.js
+++ src/App.js
@@ -8,14 +8,10 @@ function App() {
   <>  
 
     <NavBar/> 
-    <br>
-    </br>
-    <br>
-      
-    </br>
+   
     <div className="app">
     <h1>Bombay Velas</h1>
-    <h2>esto es un subtitulo</h2>
+    <h2>Tienda de velas artesanales</h2>
     <p>lorem esto es un texto de relleno</p>
     </div>
 
